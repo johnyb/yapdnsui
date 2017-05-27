02a911d2a59e813dfe095723f5ba40c274f58b6c
Julian Bäume <julian@svg4all.de>
2017-05-27 12:47:45 +0200

    delegate more props and events from link-button to b-button
    edit record buttons have been broken.

--- ui/components/icon-button.vue
+++ ui/components/icon-button.vue
@@ -1,5 +1,5 @@
 <template>
-<b-button :variant="variant" :size="size" class="d-flex align-items-center">
+<b-button :to="to" :href="href" :variant="variant" :size="size" @click="emitClick" class="d-flex align-items-center">
     <icon :label="iconLabel" :name="icon" /><slot></slot>
 </b-button>
 </template>
@@ -7,7 +7,12 @@
 <script>
 export default {
     name: 'icon-button',
-    props: ['size', 'iconLabel', 'icon', 'variant']
+    props: ['to', 'href', 'size', 'iconLabel', 'icon', 'variant'],
+    methods: {
+        emitClick: function (e) {
+            this.$emit('click', e);
+        }
+    }
 };
 </script>
 
