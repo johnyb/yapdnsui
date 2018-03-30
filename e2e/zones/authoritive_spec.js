609843288973b8bb2dcd77a48266952694776ed1
Julian Bäume <julian@svg4all.de>
2018-03-30 22:36:31 +0200

    add e2e test for slave zones
    connects a previously defined master zone on a pdns server configured as
    slave.

--- e2e/zones/authoritive_spec.js
+++ e2e/zones/authoritive_spec.js
@@ -35,9 +35,51 @@ Scenario('add a master zone', (I) => {
     I.see('zones in pdns:8081');
 });
 
-Scenario('remove zones', async (I) => {
+const util = require('util');
+const dns = require('dns');
+const lookup = util.promisify(dns.lookup);
+
+Scenario('add slave zone', async (I) => {
+    I.addServer({
+        url: 'http://pdns_slave:8081',
+        password: 'mimimi'
+    });
+    I.wait('0.5');
+    I.click('PDNS Servers');
+    I.click('pdns_slave:8081', '.dropdown-menu.show');
+    I.see('zones in pdns_slave:8081');
+
+    I.click('Create zone');
+    I.see('Add zone');
+    I.fillField('Zone/Domain name', 'yapdnsui.test.');
+
+    I.dontSee('Zone master', 'label');
+
+    I.checkOption('Slave');
+
+    const masterIP = await lookup('pdns');
+    I.see('Zone master', 'label');
+    I.fillField('Zone master', masterIP.address);
+
+    I.click('Add zone');
+    I.see('zones in pdns_slave:8081');
+});
+
+Scenario('remove zones', (I) => {
     I.amOnPage('/#/servers');
     I.click('pdns:8081', '#servers-table');
+
+    I.click('#zones-table .btn-toolbar[data-zone="yapdnsui.test."] button > [aria-label="Remove Zone"]');
+    I.waitForText('Delete zone');
+    //TODO: may be, get rid of this hard-coded timeout
+    I.wait(0.5);
+    I.click('Delete zone');
+
+    I.wait(0.5);
+
+    I.click('PDNS Servers');
+    I.wait(0.5);
+    I.click('pdns_slave:8081', '.dropdown-menu.show');
 
     I.click('#zones-table .btn-toolbar[data-zone="yapdnsui.test."] button > [aria-label="Remove Zone"]');
     I.waitForText('Delete zone');
