c724c431a520d60c3b479698341fa72d43672b02
Julian Bäume <julian@svg4all.de>
2018-03-31 16:50:28 +0200

    cleanup server_spec a bit
    some refactoring for better readability

--- e2e/server_spec.js
+++ e2e/server_spec.js
@@ -2,42 +2,51 @@
 
 Feature('API endpoints');
 
-Scenario('add a primary server', (I) => {
+function addServerFn({ url, password }) {
+    const I = this;
     I.amOnPage('/');
     I.click('PDNS Servers');
     I.click('Configure …');
     I.click('Add server');
 
-    I.fillField('URL', 'http://pdns:8081');
-    I.fillField('API-Key', 'mimimi');
+    I.fillField('URL', url);
+    I.fillField('API-Key', password);
     I.click('Add Server');
 
-    I.waitForText('pdns:8081');
+    const name = url.replace(/^http:\/\//, '');
+    I.waitForText(name);
 
-    I.see('pdns:8081', '#servers-table tr td:nth-child(1)');
-});
+    I.see(name, '#servers-table tr td:nth-child(1)');
 
-Scenario('add a secondary server', (I) => {
-    I.addServer({
+    return { name, url, password };
+}
+
+function removeSelectorFor() {
+    //return `#servers-table .btn-toolbar[data-server-name="${ name }"] [aria-label="Remove Server"]`;
+    return '#servers-table tr:nth-child(1) button > [aria-label="Remove Server"]';
+}
+
+Scenario('add and remove 3 API endpoints of different types', (I) => {
+    const addServer = addServerFn.bind(I);
+    let name;
+    name = addServer({
+        url: 'http://pdns:8081',
+        password: 'mimimi'
+    }).name;
+    I.click(removeSelectorFor(name));
+    I.dontSee(name, '#servers-table');
+
+    name = addServer({
         url: 'http://pdns_slave:8081',
         password: 'mimimi'
-    });
-});
+    }).name;
+    I.click(removeSelectorFor(name));
+    I.dontSee(name, '#servers-table');
 
-Scenario('add a recursor server', (I) => {
-    I.addServer({
+    name = addServer({
         url: 'http://pdns_rec:8082',
         password: 'mimimi'
-    });
-});
-
-Scenario('remove all servers', async (I) => {
-    I.amOnPage('/#/servers');
-
-    const servers = await I.grabNumberOfVisibleElements('#servers-table tbody tr');
-    for (let i = 0; i < servers; i++) {
-        I.click('#servers-table tr:nth-child(1) button > [aria-label="Remove Server"]');
-    }
-    I.wait(0.1);
-    I.dontSeeElementInDOM('#servers-table tbody tr');
+    }).name;
+    I.click(removeSelectorFor(name));
+    I.dontSee(name, '#servers-table');
 });
