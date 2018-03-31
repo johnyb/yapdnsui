b543e3620a57574ad4d6bd84851e2ef28cb2da2c
Julian Bäume <julian@svg4all.de>
2018-03-31 13:25:19 +0200

    switch to JS based config for codeceptjs
    much more flexible, now. Switch easily between configs.

---
+++ e2e/codecept.conf.js
@@ -0,0 +1,38 @@
+const helpers = {
+    WebDriverIO: {
+        url: 'http://localhost:3000/',
+        host: '172.18.0.1',
+        browser: 'chrome',
+        desiredCapabilities: {
+            browserName: 'chrome',
+            chromeOptions: {
+                args: ['no-sandbox', 'start-maximized']
+            },
+            acceptSslCerts: true
+        }
+    },
+    Puppeteer: {
+        url: 'http://pdnsui:3000/',
+        chrome: {
+            args: ['--no-sandbox']
+        }
+    }
+};
+
+module.exports.config = {
+    tests: './**/*_spec.js',
+    timeout: 10000,
+    output: './output',
+    helpers: {},
+    include: {
+        I: './steps_file.js'
+    },
+    bootstrap: false,
+    mocha: {},
+    name: 'tests'
+};
+
+let backend = process.env.CODECEPT_BACKEND || 'Puppeteer';
+if (!helpers[backend]) backend = 'Puppeteer';
+
+module.exports.config.helpers[backend] = helpers[backend];
